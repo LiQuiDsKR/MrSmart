42f823b5ce50f9b10ca5a94fff391d16b81dc481
ABCSDFWLO <76253761+ABCSDFWLO@users.noreply.github.com>
2023-12-30 08:17:12 +0900

    12300817

--- Web/Care4Toolbox/src/main/java/com/care4u/toolbox/sheet/supply_tool/SupplyToolDto.java
+++ Web/Care4Toolbox/src/main/java/com/care4u/toolbox/sheet/supply_tool/SupplyToolDto.java
@@ -1,7 +1,16 @@
 package com.care4u.toolbox.sheet.supply_tool;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import com.care4u.hr.membership.Membership;
+import com.care4u.hr.membership.MembershipDto;
+import com.care4u.toolbox.Toolbox;
+import com.care4u.toolbox.ToolboxDto;
 import com.care4u.toolbox.tool.Tool;
 import com.care4u.toolbox.tool.ToolDto;
 
@@ -21,6 +30,16 @@ public class SupplyToolDto {
 	
 	private LocalDate replacementDate;
 	
+	private MembershipDto worker;
+	
+	private MembershipDto leader;
+	
+	private MembershipDto approver;
+	
+	private ToolboxDto toolbox;
+	
+	private LocalDateTime eventTimestamp;
+	
 	@Builder
 	public SupplyToolDto(long id, Tool tool, int count, LocalDate replacementDate) {
 		this.id = id;
@@ -34,6 +53,11 @@ public class SupplyToolDto {
 		this.toolDto = new ToolDto(supplyTool.getTool());
 		this.count = supplyTool.getCount();
 		this.replacementDate = supplyTool.getReplacementDate();
+		this.worker = new MembershipDto(supplyTool.getSupplySheet().getWorker());
+		this.leader = new MembershipDto(supplyTool.getSupplySheet().getLeader());
+		this.approver = new MembershipDto(supplyTool.getSupplySheet().getApprover());
+		this.toolbox = new ToolboxDto(supplyTool.getSupplySheet().getToolbox());
+		this.eventTimestamp = supplyTool.getSupplySheet().getEventTimestamp();
 	}
 	
 }
