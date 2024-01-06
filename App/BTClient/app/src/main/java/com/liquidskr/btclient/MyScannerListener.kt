ab6432eba4cb75957d94da9b8d2b08cc26a82691
Lee Jae Hyeong <littleplayer777@gmail.com>
2024-01-06 14:25:27 +0900

    01061425

--- App/BTClient/app/src/main/java/com/liquidskr/btclient/MyScannerListener.kt
+++ App/BTClient/app/src/main/java/com/liquidskr/btclient/MyScannerListener.kt
@@ -4,14 +4,9 @@ class MyScannerListener() {
     interface Listener {
         fun onTextFinished()
     }
-    private var listener: Listener? = null
-
-    fun setListener(listener: Listener) {
-        this.listener = listener
-    }
 
     fun onEnter() {
-        listener?.onTextFinished()
+
     }
 
 }
